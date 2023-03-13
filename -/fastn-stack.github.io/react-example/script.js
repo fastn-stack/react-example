82a4119f91c59c1e3c1fbbc124d92d3ae04dde6c
Arpita-Jaiswal <Arpita-Jaiswal@users.noreply.github.com>
2023-03-13 09:28:28 +0000

    deploy: 0d1207f2e494a4648d98185e43b5c39621cc8c2e

--- -/fastn-stack.github.io/react-example/script.js
+++ -/fastn-stack.github.io/react-example/script.js
@@ -1,17 +1,19 @@
 import 'https://unpkg.com/react@18/umd/react.development.js';
 import 'https://unpkg.com/react-dom@18/umd/react-dom.development.js';
 import reactToWebComponent from "https://unpkg.com/react-to-webcomponent@1.7.2/dist/react-to-webcomponent.es.js";
+import 'https://unpkg.com/prop-types@15.8.1/prop-types.min.js';
 
 const e = React.createElement;
 class LikeButton extends React.Component {
     constructor(props) {
         super(props);
         this.state = { liked: false };
+        this.data = props_data(props);
     }
 
     render() {
         if (this.state.liked) {
-            return 'You liked this.';
+            return `${this.data.name.get()} liked this.`;
         }
 
         return e(
@@ -22,6 +24,19 @@ class LikeButton extends React.Component {
     }
 }
 
+LikeButton.propTypes = {
+    name: PropTypes.string.isRequired
+}
+
 const wcChecklist = reactToWebComponent(LikeButton, React, ReactDOM, { dashStyleAttributes: true });
 
 customElements.define("x-like", wcChecklist);
+
+
+function props_data(props) {
+    let data = {};
+    for (let idx in props) {
+        data[idx] = eval(props[idx]);
+    }
+    return data;
+}
